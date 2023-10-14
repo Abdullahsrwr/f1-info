ebace242ce691658d4390d0081d901c07edb240d
Abdullah Sarwar <71032852+Abdullahsrwr@users.noreply.github.com>
2023-10-14 10:26:08 -0400

    Update f1.js

--- f1.js
+++ f1.js
@@ -3,51 +3,59 @@ fetch('http://ergast.com/api/f1/current/next.json')
     .then(data => {
         const raceData = data.MRData.RaceTable.Races[0];
         const raceName = raceData.raceName;
-        const raceDate = new Date(raceData.date);
+        const raceDate = raceData.date;
         const raceTime = raceData.time;
-        const raceDateTime = new Date(`${raceDate.toDateString()} ${raceTime}`);
+        const raceDateTime = new Date(`${raceDate}T${raceTime}`);
 
         const events = [];
 
         if (raceData.Qualifying) {
-            const qualifyingDate = new Date(raceData.Qualifying.date);
+            const qualifyingDate = raceData.Qualifying.date;
             const qualifyingTime = raceData.Qualifying.time;
             events.push({
                 name: 'Qualifying',
-                date: new Date(`${qualifyingDate.toDateString()} ${qualifyingTime}`)
+                date: new Date(`${qualifyingDate}T${qualifyingTime}`)
             });
         }
 
         if (raceData.Sprint) {
-            const sprintDate = new Date(raceData.Sprint.date);
+            const sprintDate = raceData.Sprint.date;
             const sprintTime = raceData.Sprint.time;
             events.push({
                 name: 'Sprint',
-                date: new Date(`${sprintDate.toDateString()} ${sprintTime}`)
+                date: new Date(`${sprintDate}T${sprintTime}`)
             });
         }
 
-        if (Array.isArray(raceData.FirstPractice)) {
-            raceData.FirstPractice.forEach(practice => {
-                const practiceDate = new Date(practice.date);
-                const practiceTime = practice.time;
-                events.push({
-                    name: 'Free Practice',
-                    date: new Date(`${practiceDate.toDateString()} ${practiceTime}`)
-                });
-            });
-        } else if (raceData.FirstPractice) {
-            const practiceDate = new Date(raceData.FirstPractice.date);
-            const practiceTime = raceData.FirstPractice.time;
+        if (raceData.FirstPractice) {
+            const FP1Date = raceData.FirstPractice.date;
+            const FP1Time = raceData.FirstPractice.time;
             events.push({
-                name: 'Free Practice',
-                date: new Date(`${practiceDate.toDateString()} ${practiceTime}`)
+                name: 'Free Practice 1',
+                date: new Date(`${FP1Date}T${FP1Time}`)
+            });
+        }
+        if (raceData.SecondPractice) {
+            const FP2Date = raceData.SecondPractice.date;
+            const FP2Time = raceData.SecondPractice.time;
+            events.push({
+                name: 'Free Practice 2',
+                date: new Date(`${FP2Date}T${FP2Time}`)
+            });
+        }
+        if (raceData.ThirdPractice) {
+            const FP3Date = raceData.ThirdPractice.date;
+            const FP3Time = raceData.ThirdPractice.time;
+            events.push({
+                name: 'Free Practice 3',
+                date: new Date(`${FP3Date}T${FP3Time}`)
             });
         }
 
         events.sort((a, b) => a.date - b.date);
 
-        let raceInfo = `Upcoming: ${raceName}<br><br>Race: ${raceDateTime.toDateString()} ${raceDateTime.toLocaleTimeString()}`;
+        let raceInfo = `<span style="font-size: 30px;">${raceName}</span><br><br>Race: ${raceDateTime.toDateString()} ${raceDateTime.toLocaleTimeString()}`;
+        raceInfo += `<br><br>---------------------------------`;
         events.forEach(event => {
             raceInfo += `<br><br>${event.name}: ${event.date.toDateString()} ${event.date.toLocaleTimeString()}`;
         });
